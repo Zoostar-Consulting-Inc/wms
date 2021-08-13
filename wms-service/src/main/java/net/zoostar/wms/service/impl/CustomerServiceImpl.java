69c75154a7ebb37c2f57e3ec3e2ac8e92ea7bd0a
zoostar <devops@zoostar.net>
2021-08-13 11:33:33 -0400

    WMS-1: Add transactional capabilities

--- wms-service/src/main/java/net/zoostar/wms/service/impl/CustomerServiceImpl.java
+++ wms-service/src/main/java/net/zoostar/wms/service/impl/CustomerServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.TreeSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.extern.slf4j.Slf4j;
 import net.zoostar.wms.dao.CustomerRepository;
@@ -13,13 +14,15 @@ import net.zoostar.wms.model.Customer;
 import net.zoostar.wms.service.CustomerService;
 
 @Slf4j
-@Service	
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
 	protected CustomerRepository customerRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public Set<Customer> search(Set<String> searchTerms) {
 		log.info("Search by: {}", searchTerms.toString());
 		Set<Customer> customers = new TreeSet<>();
@@ -38,6 +41,7 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Customer retrieveByEmail(String email) {
 		log.info("Search for email: {}...", email);
 		var entity = customerRepository.findByEmail(email);
